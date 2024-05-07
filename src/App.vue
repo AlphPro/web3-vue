19ced12e65274813df3594baa33af94ececac415
not-reed <reed@degen.watch>
2024-05-07 14:04:20 -0700

    update dependencies and fix autoconnect error

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import { ref } from "vue";
+  import {
+    AlephiumConnectionProvider,
+    AlephiumConnect,
+    useAccount
+  } from "./";
 
-const counter = ref(0)
+  const { account } = useAccount()
 </script>
 
 <template>
-  <button @click="counter++"> Clicked me {{ counter }} times </button>
+  <AlephiumConnectionProvider :autoConnect="true" :group="0" network="mainnet">
+    <AlephiumConnect v-slot="{ isConnected, connectExtension, connectDesktop, disconnect }">
+      <template v-if="!isConnected">
+        <button @click="connectExtension">Extension</button>
+        <button @click="connectDesktop">Desktop</button>
+      </template>
+      <button v-else @click="disconnect">Disconnect</button>
+    </AlephiumConnect>
+
+    {{ account.address }}
+  </AlephiumConnectionProvider>
 </template>
